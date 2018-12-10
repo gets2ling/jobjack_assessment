930d9bfbe4101dd2771c3724bfb3be513429c74e
gets2ling <33947806+gets2ling@users.noreply.github.com>
2018-12-10 07:05:51 +0200

    Added path not found error handling

--- restAPI_server/service.js
+++ restAPI_server/service.js
@@ -5,7 +5,7 @@ var ReadStream = require('./readStream');
 
 exports.directoryRequest = function (req, res) {
     const reqUrl = url.parse(req.url, true);
-    var defaultPath = 'c:/photos';
+    var defaultPath = 'c:/';
     if (reqUrl.query.dir) {
         defaultPath = reqUrl.query.dir
     };
@@ -21,6 +21,17 @@ exports.directoryRequest = function (req, res) {
     readStream
         .on('data', function (chunk) {            
             file[filekey].push(chunk);          
+        })
+        .on('error', function (err) {            
+            console.log("filepath: '"+err.path+ "' has error code: " +err.code);
+            
+            var response = {
+                "file_error": "filepath: '"+err.path+ "' has error code: " +err.code
+            }
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response))
         })
         .on('end', function () {  // done
 
